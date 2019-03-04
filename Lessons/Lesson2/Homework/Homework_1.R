a5ddc7b87af37bff961d4aaf15089d696bce5a3b
hanicova <47779306+hanicova@users.noreply.github.com>
2019-03-04 21:03:03 +0100

    Update Homework_1.R

--- Lessons/Lesson2/Homework/Homework_1.R
+++ Lessons/Lesson2/Homework/Homework_1.R
@@ -25,16 +25,13 @@ dt_KPI_raw %>%
   ggplot(aes(x = reorder(Year, UWR), y = UWR)) + 
   geom_col()
 
-# neviem preco, ale neviem z mojho R-studia push-nut kod na git, ked som ho sem vlozila a v R-studiu som dala pull vsetko zbehlo ako malo, 
-# ked som nasledne spravila zmeny v R-studiu a dala push, vypisalo mi ze je vsetko up to date, ale na gite ziadne zmeny nespravil
+# neviem preco, ale neviem z mojho R-studia push-nut kod na git, ked som ho sem vlozila a 
+# v R-studiu som dala pull vsetko zbehlo ako malo, ked som nasledne spravila zmeny v R-studiu
+# a dala push, vypisalo mi ze je vsetko up to date, ale na gite ziadne zmeny nespravil
 # 
-
-
-
 
 # Your Explanation about analysis:
 # 
-Vycistenie dat -> roztriedenie po rokoch -> porovnanie s Underwriting Result
-Najmensie UWR v roku 2015 -> 2015 najhorsi rok
-# 
-# 
+# Vycistenie dat -> roztriedenie po rokoch -> porovnanie s Underwriting Result
+# Najmensie UWR v roku 2015 -> 2015 najhorsi rok
+#
