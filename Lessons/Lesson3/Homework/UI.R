4b80ce87c5292d18271b3b8a82c5d230aac0e8fd
hanicova <47779306+hanicova@users.noreply.github.com>
2019-03-11 22:38:22 +0100

    Delete UI.R

--- Lessons/Lesson3/Homework/UI.R
+++
@@ -1,8 +0,0 @@
-UI <- fluidPage(
-  selectInput(inputId = "chosen_par", 
-              label = "Graph", 
-              choices = list("Region", "Unit", "Segment", "Business", "Year")),
-  plotOutput(outputId = "graph")
-) 
-
-# v serveri chceme menit parameter (input bude z ui) podla ktoreho sa robia zoskupenia
